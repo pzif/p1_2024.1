b94fbcafbd7630513fbdf1489e6c7a13296e995b
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 13:10:13 -0300

    Definindo que cada email só pode se cadastrar uma vez

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,6 +6,15 @@ const router = express.Router();
 
 router.post('/register',async(req,res) => {
 
+  const {email} = req.body
+  
+  if(await UserModel.findOne({email})){
+    return res.status(400).json({
+      error: true,
+      message:'Usuário já cadastrado'
+    })
+  }
+
   const User = await UserModel.create(req.body);
 
    return res.json({
