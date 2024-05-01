55a60212aba5d3c8b96b76ff9710c93693ceef52
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 01:50:20 -0300

    Removendo avisos de depreciação

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://pz47_user:Tu8i@cluster0.rxkuh4u.mongodb.net/banco-p1?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true, // Adicionando esta opção para evitar avisos de depreciação
-  useUnifiedTopology: true // Adicionando esta opção para evitar avisos de depreciação
-})
+mongoose.connect('mongodb+srv://pz47_user:Tu8i@cluster0.rxkuh4u.mongodb.net/banco-p1?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
   console.log('Conexão com o MongoDB estabelecida com sucesso!');
 })
@@ -14,3 +11,4 @@ mongoose.connect('mongodb+srv://pz47_user:Tu8i@cluster0.rxkuh4u.mongodb.net/banc
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
+
