cd16e698b173db0b1a3d12d3efe821132bfd9b1b
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 02:30:21 -0300

    Primeiro usuário cadastrado

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,11 +4,14 @@ const UserModel = require('../models/user');
 
 const router = express.Router();
 
-router.post('/register',(req,res) => {
-  console.log(req.body);
-  return res.json({
+router.post('/register',async(req,res) => {
+
+  const User = await UserModel.create(req.body);
+
+   return res.json({
     error: false,
-    message:'Registrado com sucesso!'
+    message:'Registrado com sucesso!',
+    data: User
   });  
 })
 
