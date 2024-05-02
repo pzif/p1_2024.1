24432476b2bef0d518300fc561c397ae3b7694f5
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 22:18:04 -0300

    AdminController

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const AuthController = require('./controllers/AuthController');
-
+const AdminController = require('./controllers/AdminController');
 const app = express();
 
 app.use(express.json());
 
 app.use('/auth', AuthController)
+app.use('/admin', AdminController);
 
 app.listen(3001, () =>{
     console.log('O servidor está rodando.');
