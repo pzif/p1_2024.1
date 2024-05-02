41d047d90c0f274106a0fa28bd3425ce1811bbeb
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 22:00:52 -0300

    token para novos cadastros

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
@@ -7,63 +7,60 @@ const UserModel = require('../models/user');
 
 const router = express.Router();
 
-router.post('/register',async(req,res) => {
-
-  const {email} = req.body
-  
-  if(await UserModel.findOne({email})){
-    return res.status(400).json({
-      error: true,
-      message:'Usuário já cadastrado'
-    })
-  } 
-
-  const User = await UserModel.create(req.body);
-
-  User.password = undefined;
-
-   return res.json({
-    error: false,
-    message:'Registrado com sucesso!',
-    data: User
-  });  
-})
-
-router.post('/authenticate', async(req, res)=>{
-
-  const{email, password} = req.body;
-
-  const user = await UserModel.findOne({email}).select('+password');
-
-  if(!user){
-    return res.status(400).json({
-      error: true,
-      message: 'Usuário não encontrado.'
-    })
-  }
-
-  if(!await bcrypt.compare(password, user.password)){
-    return res.status(400).send({
-      error: true,
-      message: 'Senha inválida!'
-    })
-  }
-
-  user.password = undefined;
-
-  const token = jwt.sign({
+const generateToken = (user = {}) => {
+  return jwt.sign({
     id: user.id,
     name: user.name
   }, authConfig.secret , {
     expiresIn: 86400
-
   });
+};
+
+router.post('/register', async (req, res) => {
+  const { email } = req.body;
+  
+  if (await UserModel.findOne({ email })) {
+    return res.status(400).json({
+      error: true,
+      message: 'Usuário já cadastrado'
+    });
+  } 
+
+  const user = await UserModel.create(req.body);
+
+  user.password = undefined;
 
   return res.json({
-    user,
-    token
+    user: user,
+    token: generateToken(user)
   });
+});
 
-})
+router.post('/authenticate', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email }).select('+password');
+
+  if (!user) {
+    return res.status(400).json({
+      error: true,
+      message: 'Usuário não encontrado.'
+    });
+  }
+
+  if (!await bcrypt.compare(password, user.password)) {
+    return res.status(400).send({
+      error: true,
+      message: 'Senha inválida!'
+    });
+  }
+
+  user.password = undefined;
+
+  return res.json({
+    user: user,
+    token: generateToken(user)
+  });
+});
 
 module.exports = router;
