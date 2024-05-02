d74c868fc90d2b7a861fa168a2345cd3ff9751b6
pzif <82480187+pzif@users.noreply.github.com>
2024-05-01 21:07:01 -0300

    Autenticação

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const bcrypt = require('bcryptjs');
 
 const UserModel = require('../models/user');
 
@@ -13,7 +14,7 @@ router.post('/register',async(req,res) => {
       error: true,
       message:'Usuário já cadastrado'
     })
-  }
+  } 
 
   const User = await UserModel.create(req.body);
 
@@ -24,6 +25,32 @@ router.post('/register',async(req,res) => {
     message:'Registrado com sucesso!',
     data: User
   });  
+})
+
+router.post('/authenticate', async(req, res)=>{
+
+  const{email, password} = req.body;
+
+  const user = await UserModel.findOne({email}).select('+password');
+
+  if(!user){
+    return res.status(400).json({
+      error: true,
+      message: 'Usuário não encontrado.'
+    })
+  }
+
+  if(!await bcrypt.compare(password, user.password)){
+    return res.status(400).send({
+      error: true,
+      message: 'Senha inválida!'
+    })
+  }
+
+  user.password = undefined;
+
+  return res.json(user);
+
 })
 
 module.exports = router;
